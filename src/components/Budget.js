c36d063b35c68b91535a596dbf3acea10e877995
SeWillz <138167827+SeWillz@users.noreply.github.com>
2023-08-15 14:00:38 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, setBudget, expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    const remainingBudget = budget - totalExpenses;
+    const upperLimit = 20000;
+
+    const [inputBudget, setInputBudget] = useState(budget);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleBudgetChange = (event) => {
+        const newBudget = parseFloat(event.target.value);
+        if (!isNaN(newBudget) && newBudget <= remainingBudget && newBudget <= upperLimit) {
+            setInputBudget(newBudget);
+            setErrorMessage('');
+        } else if (newBudget > upperLimit) {
+            const shouldUpdate = window.confirm(
+                `The budget you entered exceeds the upper limit of £${upperLimit}. Do you want to proceed?`
+            );
+
+            if (shouldUpdate) {
+                setInputBudget(newBudget);
+                setErrorMessage('');
+                setBudget(newBudget); // Update the actual budget value
+            } else {
+                setInputBudget(budget);
+            }
+        } else {
+            setInputBudget(newBudget);
+            setErrorMessage('Budget cannot exceed remaining budget or £20,000.');
+        }
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                type='number'
+                value={inputBudget}
+                onChange={handleBudgetChange}
+                step='10'
+                min='0'
+                max={upperLimit}
+                className={`budget-input ${errorMessage && 'error'}`}
+            />
+            {errorMessage && <p className='error-message'>{errorMessage}</p>}
         </div>
     );
 };
+
 export default Budget;
